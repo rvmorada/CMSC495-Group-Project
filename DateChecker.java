2c69bcc10c3dcda16cfa41b1c0601900c23a8f82
rvmorada <rvmorada@gmail.com>
2019-09-27 16:54:50 -0400

    9/27/2019 Commit
    We can now add dates through the application.
    - Added DateAdder class.
    - Added displayMessage(String) method to Display class.
    - Added addDatePanel to Display class.

--- DateChecker.java
+++ DateChecker.java
@@ -3,14 +3,7 @@ import java.text.DateFormat;
 import java.text.ParseException;
 
 public class DateChecker 
-{
-//	private String dateFormat;
-//	 
-//    public DateChecker(String dateFormat) {
-//        this.dateFormat = dateFormat;
-//    }
- 
-    
+{    
     public boolean isValidDate(String dateStr) {
         DateFormat simpleDateFormat = new SimpleDateFormat("MM/dd/yyyy");
         simpleDateFormat.setLenient(false);
